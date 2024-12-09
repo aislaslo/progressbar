447e651f551083b0e559f2d3a4c0d035b938dec0
aislaslo <islas.alex@gmail.com>
2024-12-09 16:48:28 -0600

    dawn progress

---
+++ main.py
@@ -0,0 +1,15 @@
+from rich.progress import Progress, SpinnerColumn, TimeElapsedColumn
+
+import time
+
+progress = Progress(*Progress.get_default_columns(), TimeElapsedColumn(), SpinnerColumn())
+
+with progress:
+
+    task = progress.add_task("Downloading...",total=100)
+
+    while not progress.finished:
+    
+        progress.update(task, advance=10)
+
+        time.sleep(1)
